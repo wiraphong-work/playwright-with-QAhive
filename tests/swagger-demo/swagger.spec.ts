52c88488315074c0f391016940a95595b5fd4dd2
wiraphong-work <wiraphong.ch.work@gmail.com>
2024-03-13 15:27:44 +0700

    comment variabl not use

--- tests/swagger-demo/swagger.spec.ts
+++ tests/swagger-demo/swagger.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, request } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import { ApiTesting } from "../component/global-fuction";
 
 const data_regis = {
@@ -10,7 +10,7 @@ const data_regis = {
 
 const component = new ApiTesting();
 let access_token;
-let access_token_example = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRlbW8wMUBkZW1vLmNvbSIsInJvbGVzIjpbXSwiaWF0IjoxNzEwMzE2MjQxLCJleHAiOjE3MTA0ODkwNDF9.xuZbt06OGBPKZOvrTXmtep_yc63hxB8KunqiTMepDx8';
+//let access_token_example = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRlbW8wMUBkZW1vLmNvbSIsInJvbGVzIjpbXSwiaWF0IjoxNzEwMzE2MjQxLCJleHAiOjE3MTA0ODkwNDF9.xuZbt06OGBPKZOvrTXmtep_yc63hxB8KunqiTMepDx8';
 const baseURL = 'https://api-web-demo.qahive.com'
 
 //เล่นได้ 3 API
