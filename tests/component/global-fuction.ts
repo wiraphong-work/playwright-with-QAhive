61b21f57bb35430f026be5c2dd76a61bf294e5e9
wiraphong-work <wiraphong.ch.work@gmail.com>
2024-03-15 10:17:33 +0700

    Create testcase data generate some value

--- tests/component/global-fuction.ts
+++ tests/component/global-fuction.ts
@@ -1,26 +1,57 @@
-import { Page , expect } from '@playwright/test';
-
+import { Page, expect } from '@playwright/test';
 
 export class MainPage {
-    constructor(private readonly page: Page) { }
+  constructor(private readonly page: Page) {}
 
-    async gotoPage() {
-        await this.page.goto('todo-list');
-      }
+  async gotoPage() {
+    await this.page.goto('todo-list');
+  }
 
-    async addListTodo(value: any[]) {
-      for (const list of await value) {
-        await this.page.getByTestId("inputTodo").fill(list);
-        await this.page.getByText("submit").click();
-      }
+  async addListTodo(value: any[]) {
+    for (const list of await value) {
+      await this.page.getByTestId('inputTodo').fill(list);
+      await this.page.getByText('submit').click();
     }
+  }
 
-}  
+  async getRandom(arr, n) {
+    var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+    if (n > len)
+      throw new RangeError('getRandom: more elements taken than available');
+    while (n--) {
+      var x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+  }
 
-export class ApiTesting{
-  async checkKeyOfJson( responseBody: JSON ,arr: any[]) {
-    await arr.forEach((val)=>{
-     expect(responseBody).toHaveProperty(val);
-     })
- }
+  async clearCheckbox() {
+    // const clear_data = await [
+    //   'titleEN',
+    //   'firstNameEN',
+    //   'lastNameEN',
+    //   'email',
+    //   'citizenId',
+    // ];
+    // await clear_data.map((val) => {
+    //   this.page.click(`[name="${val}"]`);
+    // });
+
+    await this.page.click(`[name="titleEN"]`);
+    await this.page.click(`[name="firstNameEN"]`);
+    await this.page.click(`[name="lastNameEN"]`);
+    await this.page.click(`[name="email"]`);
+    await this.page.click(`[name="citizenId"]`);
+  }
+}
+
+export class ApiTesting {
+  async checkKeyOfJson(responseBody: JSON, arr: any[]) {
+    await arr.forEach((val) => {
+      expect(responseBody).toHaveProperty(val);
+    });
+  }
 }
--- tests/datagenerator-demo/detagen.spec.ts
+++ tests/datagenerator-demo/detagen.spec.ts
@@ -1,115 +1,106 @@
 import { test, expect } from '@playwright/test';
+import { MainPage } from '../component/global-fuction';
 
 const data_check = [
-    {
-        key: 'titleEN',
-        name: 'Title'
-    },
-    {
-        key: 'firstNameEN',
-        name: 'First Name'
-    },
-    {
-        key: 'lastNameEN',
-        name: 'Last Name'
-    },
-    {
-        key: 'fullNameEN',
-        name: 'Full Name'
-    },
-    {
-        key: 'genderEN',
-        name: 'Gender'
-    },
-    {
-        key: 'dateOfBirthEN',
-        name: 'Date Of Birth'
-    },
-    {
-        key: 'age',
-        name: 'Age'
-    },
-    {
-        key: 'email',
-        name: 'Email'
-    },
-    {
-        key: 'phoneNo',
-        name: 'Phone no.'
-    },
-    {
-        key: 'citizenId',
-        name: 'Citizen Id'
-    },
+  {
+    key: 'titleEN',
+    name: 'Title',
+  },
+  {
+    key: 'firstNameEN',
+    name: 'First Name',
+  },
+  {
+    key: 'lastNameEN',
+    name: 'Last Name',
+  },
+  {
+    key: 'fullNameEN',
+    name: 'Full Name',
+  },
+  {
+    key: 'genderEN',
+    name: 'Gender',
+  },
+  {
+    key: 'dateOfBirthEN',
+    name: 'Date Of Birth',
+  },
+  {
+    key: 'age',
+    name: 'Age',
+  },
+  {
+    key: 'email',
+    name: 'Email',
+  },
+  {
+    key: 'phoneNo',
+    name: 'Phone no.',
+  },
+  {
+    key: 'citizenId',
+    name: 'Citizen Id',
+  },
 ];
+let component;
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('/test-data-generator');
+  await page.goto('/test-data-generator');
 });
 
 test('Generate all', async ({ page }) => {
-    await data_check.map(async (value) => {
-        await page.check(`[name="${value.key}"]`, { force: true });
-    });
-    await page.getByRole('button', { name: 'generate' }).click();
-    const rowTitle = await page
-        .locator('.MuiDataGrid-columnHeaderTitle')
-        .allTextContents();
-    await expect(data_check.map((val)=> val.name)).toEqual(expect.arrayContaining(rowTitle));
-
-    // await data_check.map(async(val,index)=>{
-    //     await expect(val.name).toEqual(rowTitle[index]);
-    // })
+  await data_check.map(async (value) => {
+    await page.check(`[name="${value.key}"]`, { force: true });
+  });
+  await page.getByRole('button', { name: 'generate' }).click();
+  const rowTitle = await page
+    .locator('.MuiDataGrid-columnHeaderTitle')
+    .allTextContents();
+  await expect(data_check.map((val) => val.name)).toEqual(
+    expect.arrayContaining(rowTitle)
+  );
 });
 
-// test('Generate some', async ({ page }) => {
-    // function getRandom(arr, n) {
-    //     var result = new Array(n),
-    //         len = arr.length,
-    //         taken = new Array(len);
-    //     if (n > len)
-    //         throw new RangeError("getRandom: more elements taken than available");
-    //     while (n--) {
-    //         var x = Math.floor(Math.random() * len);
-    //         result[n] = arr[x in taken ? taken[x] : x];
-    //         taken[x] = --len in taken ? taken[len] : len;
-    //     }
-    //     return result;
-    // }
-//     const data_random_check = await getRandom(data_check.map((val)=>{return val.key}) ,  Math.floor(Math.random() * 9))
-//     await console.log('data_random_check_1:',data_random_check)
-//     await data_random_check.map(async (value) => {
-//         await console.log('value 1:',value)
-//         await page.check(`[name="${value}"]`, { force: true });
-//     });
-//     await page.getByRole('button', { name: 'generate' }).click();
-//     const rowTitle = await page
-//         .locator('.MuiDataGrid-columnHeaderTitle')
-//         .allTextContents();
-//     await console.log('data_random_check_2:',data_random_check)
-//     await console.log('rowTitle:' , rowTitle)
+test('Generate some', async ({ page }) => {
+  component = new MainPage(page);
+  await component.clearCheckbox();
+  const data_random_check = await component.getRandom(
+    data_check.map((val) => {
+      return val.key;
+    }),
+    Math.floor(Math.random() * 9)
+  );
+  await data_random_check.map(async (value) => {
+    await page.check(`[name="${value}"]`, { force: true });
+  });
+  await page.getByRole('button', { name: 'generate' }).click();
+  const rowTitle = await page
+    .locator('.MuiDataGrid-columnHeaderTitle')
+    .allTextContents();
+  const data_map = await data_check.filter((e) => {
+    return data_random_check.includes(e.key);
+  });
+  await expect(data_map.map((val) => val.name)).toEqual(
+    expect.arrayContaining(rowTitle)
+  );
+});
 
-//     await data_random_check.map(async(val , index)=>{
-//         console.log('val:',val)
-//         // await expect(val).toContain(rowTitle[index]);
-//     })
-//  });
-
-test('Check data records', async ({ page }) => { 
-    const number_random = await Math.floor(Math.random() * 25).toString()
-    await page.fill('.MuiInputBase-input', number_random)
-    await page.getByRole('button', { name: 'generate' }).click();
-    const number_row = await page.locator('.MuiTablePagination-displayedRows').textContent()
-    await expect(number_row).toContain(number_random)
+test('Check data records', async ({ page }) => {
+  const number_random = await Math.floor(Math.random() * 25).toString();
+  await page.fill('.MuiInputBase-input', number_random);
+  await page.getByRole('button', { name: 'generate' }).click();
+  const number_row = await page
+    .locator('.MuiTablePagination-displayedRows')
+    .textContent();
+  await expect(number_row).toContain(number_random);
 });
 
 test('Export excel', async ({ page }) => {
-    const downloadPromise = page.waitForEvent('download');
-    await page.getByText('Export').click();
-    await page.getByText('Download as CSV').click();
-    const download = await downloadPromise;
-    const result = await download.failure();
-    await expect(result).toBeNull()
- });
-
-
+  const downloadPromise = page.waitForEvent('download');
+  await page.getByText('Export').click();
+  await page.getByText('Download as CSV').click();
+  const download = await downloadPromise;
+  const result = await download.failure();
+  await expect(result).toBeNull();
+});
